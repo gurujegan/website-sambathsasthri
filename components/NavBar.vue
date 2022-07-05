604e4a9426df3c78afaf7a97d43a0de01e7a3a39
gurujegan <gurujegan.nathan@gmail.com>
2022-07-05 16:37:03 +0530

    fixed navbar alignment on new component

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    <nav id="navbar" class="sticky top-0 right-0 left-0 h-20 bg-yellow-900 flex justify-between items-center py-4 px-6 shadow-2xl">
+    <nav id="navbar" class="h-20 bg-yellow-900 flex justify-between items-center py-4 px-6 shadow-2xl">
      <NuxtLink to="/home" class="logo ml-36 pl-2">
-        <h1 class="text-white">
+        <h1 class="text-white text-3xl">
           <span class="fname capitalize">SambathSastri</span>
           <span class="fist">&#x1F44A;</span>
         </h1>
       </NuxtLink>
-      <ul class="fixed right-44 font-bold text-yellow-900">
+      <ul class="fixed flex justify-between items-center right-44 font-bold text-yellow-900">
         <li class="nav-item h-10 ml-8 rounded-lg hover:bg-white  bg-yellow-300 text-center pt-2 w-32">
          <NuxtLink to="/home" class="block hover:text-blue-400" id="nav-link">Home</NuxtLink>
         </li>
@@ -29,3 +29,35 @@
     </nav>
   </div>
 </template>
+
+<style>
+.sticky {
+  position: fixed;
+  top: 0;
+  width: 100%;
+  z-index: 1;
+}
+</style>
+
+<script>
+export default {
+  mounted () {
+    window.onscroll = function () {
+      myFunction()
+    }
+    // Get the navbar
+    const navbar = document.getElementById('navbar')
+    // Get the offset position of the navbar
+    const sticky = navbar.offsetTop
+
+    // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+    function myFunction () {
+      if (window.pageYOffset >= sticky) {
+        navbar.classList.add('sticky')
+      } else {
+        navbar.classList.remove('sticky')
+      }
+    }
+  }
+}
+</script>
